dbd579526ff6bd66c64e3af52117e18988f60de1
AdamSpannbauer <spannbaueradam@gmail.com>
2020-12-21 15:55:24 -0500

    proj setup

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const canvasW = 512;
+const canvasH = 512;
+
+function setup() {
+  createCanvas(canvasW, canvasH);
+}
+
+function draw() {
+  background(200);
+}
+
+window.setup = setup;
+window.draw = draw;
