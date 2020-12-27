33d370c2502294b33b5c6a5bde1bf51f1f9f0f37
Jayson Rice <jayson.rice.95@gmail.com>
2020-12-27 16:18:30 -0500

    Boat rotates.

--- src/boat.js
+++ src/boat.js
@@ -15,9 +15,13 @@ export default class Boat {
     push();
     const yAdj = 3 * sin(frameCount * 0.03);
 
+    const angle = cos(frameCount * 0.03) * 0.15;
+
     if (this.img) {
       imageMode(CENTER);
-      image(this.img, this.x, this.y + yAdj - this.h * 0.3, this.w, this.h);
+      translate(this.x, this.y);
+      rotate(angle);
+      image(this.img, 0, 0 + yAdj - this.h * 0.3, this.w, this.h);
     } else {
       ellipse(this.x, this.y + yAdj, 50);
     }
